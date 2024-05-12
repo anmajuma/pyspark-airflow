ba480121b732633096a62349958df0da9bcb4aea
Your Name <you@example.com>
2024-05-13 03:54:44 +0530

    commit

--- spark/lakehouse/utils/sparkSessionBuilder.py
+++ spark/lakehouse/utils/sparkSessionBuilder.py
@@ -1,19 +1,10 @@
 from pyspark.sql import SparkSession
-from delta import configure_spark_with_delta_pip
+
 
 def createSparkSession():
-    # spark = SparkSession.builder \
-    #     .appName(app_name) \
-    #     .enableHiveSupport() \
-    #     .getOrCreate()
-    # spark.sparkContext.setLogLevel('ERROR')
-    # return spark
-
-    builder = (
-    SparkSession.builder
-        .appName('lakeHouseApp')
-        .config('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension')
-        .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog')
-    )
-    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    spark = SparkSession.builder \
+        .appName('lakeHouseApp') \
+        .enableHiveSupport() \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
     return spark
