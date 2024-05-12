82ca641c6e519eea1aab25a3edbd63f7ab80f1dc
Your Name <you@example.com>
2024-05-12 20:02:38 +0530

    commit

--- dags/create_etl.py
+++ dags/create_etl.py
@@ -36,6 +36,7 @@ with DAG(
             --executor-cores 2 \
             --driver-cores 1\
             --driver-memory 1g \
+            --py-files /opt/bitnami/spark/dist/lakehouse-0.1-py3-none-any.whl
             /opt/bitnami/spark/dev/scripts/driver.py
             '''
     )
