a1418bec2b1d57a4d341fb4f0f0259359deb19b2
Your Name <you@example.com>
2024-05-12 19:00:13 +0530

    commit

---
+++ dags/create_etl.py
@@ -0,0 +1,46 @@
+from airflow import DAG 
+from airflow.providers.ssh.operators.ssh import SSHOperator
+
+from airflow.operators.bash import BashOperator
+from airflow.hooks.base import BaseHook
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner' : 'John Doe',
+    'retries' : 1,
+    'retry_delay' : timedelta(minutes=1),
+    'depends_on_past' : True
+}
+
+
+
+with DAG(
+    dag_id='ingest_Colors_data_from_Landing_Zone',
+    description='creates ETL pipeline for Colors Data Set',
+    start_date=datetime(year=2022, month=12, day=21, hour=1, minute=30),
+    schedule_interval=timedelta(hours=1),
+    catchup=False,
+    max_active_runs=1
+) as dag:
+    
+    # task1 = BashOperator(task_id='greeting', bash_command='whoami')
+
+    task2 = SSHOperator(
+        task_id='remove_duplicate',
+        ssh_conn_id='ssh_spark',
+        command=f'''. /home/spark_user/docker_env.txt && spark-submit \
+            --master spark://spark:7077 \
+            --deploy-mode client \
+            --executor-memory 4g \
+            --executor-cores 2 \
+            --driver-cores 1\
+            --driver-memory 1g \
+            /opt/bitnami/spark/dev/scripts/driver.py
+            '''
+    )
+
+    # task3 = BashOperator(task_id='fin', bash_command='echo completed')
+
+    task2
+    # task1 >> task2 >> task3
---
+++ scripts/driver.py
@@ -0,0 +1,29 @@
+from lakehouse.utils import sparkSessionBuilder ,jobTaskIDGen , auditlog
+from lakehouse.tasks import src2landing ,removeduplicate,schemaenforcement,dataqcheck,stagecleanseddata,loadsilvertable
+
+spark = sparkSessionBuilder.createSparkSession()
+
+jobID = jobTaskIDGen.genID()
+print(jobID)
+
+aws_bucket_name = "wba"
+dataDictConfigPath = "s3a://%s/pipeline-config/schema/mongo/wwi/datadict.json" % aws_bucket_name
+dataDictMappingConfigPath = "s3a://%s/pipeline-config/schema/mongo/mongoToSparkMapping.json" % aws_bucket_name
+dqconfigpath = "s3a://%s/pipeline-config/dq" % aws_bucket_name
+auditlogPath = "s3a://%s/auditlog" % aws_bucket_name
+landingPath = "s3a://%s/landing" % aws_bucket_name
+temppath = "s3a://%s/temp" % aws_bucket_name
+brnzPath = "s3a://%s/bronze" % aws_bucket_name
+errPath = "s3a://%s/error" % aws_bucket_name
+stgpath = "s3a://%s/stage" % aws_bucket_name
+silverpath = "s3a://%s/silver" % aws_bucket_name
+sourceConnName = "wwi"
+sourceType = "postgres"
+sourceName = "wwi"
+tablenm = 'public/Colors'
+partition_columns = ["ValidFrom"]
+
+taskStatusDF = removeduplicate.cleanRawData_removeDuplicate(spark,jobID,temppath,landingPath,tablenm,sourceName,sourceType,auditlogPath,dataDictConfigPath,partition_columns)
+taskStatusDF.show()
+TaskStatus = taskStatusDF.first()['TaskStatus']
+print(TaskStatus)
